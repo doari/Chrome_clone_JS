9d0e29555cf3a7639f427327fd2571c89394fa55
민병준 <ehxhfl727@gmail.com>
2023-09-12 01:29:23 +0900

    Booleans

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const a = 5; 
-const b = 2;
-let myName = "Jun" 
- 
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello "+ myName);
-
-myName = "min"
-
-console.log("your new name is "+ myName);
+const amIFat = null;
+let someting;
+console.log(someting);
