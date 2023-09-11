a32be50b4c43d446bbcb1e5a07b2fc3163aa9332
민병준 <ehxhfl727@gmail.com>
2023-09-12 01:51:00 +0900

    Arrays

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-const amIFat = null;
-let someting;
-console.log(someting);
+// const mon ="mon";
+// const tue ="tue";
+// const wed ="wed";
+// const thu ="thu";
+// const fri ="fri";
+// const sat ="sat";
+// const sun ="sun";
+// const nonsense = [1,2,"hello",false,null,true,undefined,"Min"]
+
+const daysOfWeek = ["mon", "tue" , "wed","thu","fri","sat"];
+
+
+console.log(daysOfWeek);
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
+
+const toBuy = ["potato","tomato"];
+toBuy.push("kimbab");
+console.log(toBuy);
+
