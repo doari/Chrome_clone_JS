eb9e3bbb1bc8ce67f35fcbe8c0e0b1f17f9d0b4c
민병준 <ehxhfl727@gmail.com>
2023-09-12 02:05:54 +0900

    Objects

--- app.js
+++ app.js
@@ -1,20 +1,37 @@
-// const mon ="mon";
-// const tue ="tue";
-// const wed ="wed";
-// const thu ="thu";
-// const fri ="fri";
-// const sat ="sat";
-// const sun ="sun";
-// const nonsense = [1,2,"hello",false,null,true,undefined,"Min"]
+// 설명이 필요하지 않은 데이터 리스트들은 array로,
+// 설명이 필요한 정보가 담긴 데이터 리스트들은 object로!
 
-const daysOfWeek = ["mon", "tue" , "wed","thu","fri","sat"];
+//object는 property를 가진 데이터를 저장해주며, { } 를 사용한다.
 
+const player = {
+name : "tomato",
+color : "red",
+food : true,
+};
 
-console.log(daysOfWeek);
-daysOfWeek.push("sun");
-console.log(daysOfWeek);
+console.log(player);
 
-const toBuy = ["potato","tomato"];
-toBuy.push("kimbab");
-console.log(toBuy);
+//property를 불러오는 방법은 2가지가 있다.
+
+//1. console.log(player.name); => tomato
+//2. console.log(player["name"]); => tomato
+
+//또한 property를 바꾸는 것은 가능하지만 선언된 object를 바꾸는 것은 불가능하다.
+//ex)
+const player2 = {
+name : "tomato",
+color : "red",
+food : true,
+};
+
+console.log(player2);
+player2.color = "blue";
+console.log(player2.color);
+//--> blue
+
+//그리고 property를 추가 할 수도 있다.
+
+player2.koreanName = "토마토";
+
+//--> {name: "tomato", color: "blue", food: true, koreaName: "토마토"}
 
