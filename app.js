bcd4fa83c4ebed07c60a7ca00816feee136ffe67
민병준 <ehxhfl727@gmail.com>
2023-09-15 23:56:39 +0900

    Conditionals

--- app.js
+++ app.js
@@ -1,43 +1,14 @@
-//console.log는 그림의 떡. 꺼내 먹으려면 return필요
-//function안에서 return과 추가작업을 입력하면
-//return만 작업하고 추가 수행은 이뤄지지 않는다.
-//만약 return 앞에 기타작업이 있다면 이 작업은 수행된다.
-//즉, return"까지만" 수행된다!
-const calculator = {
-    add: function (a, b) {
-    //console.log(a + b);
-    return a + b;
-    //return에서 반환되므로 return 밑으로 쓰는 코드는 실행 안됨
-    },
-    
-    subtract: function (a, b) {
-    //console.log(a - b);
-    return a - b;
-    },
-    
-    multiply: function (a, b) {
-    //console.log(a * b);
-    return a * b;
-    },
-    
-    divide: function (a, b) {
-    //console.log(a / b);
-    return a / b;
-    },
-    
-    square: function (a, b) {
-    //console.log(a ** b);
-    return a ** b;
-    },
-    };
-    
-    let result = calculator.add(10, 2); //12
-    console.log(result);
-    result = calculator.subtract(10, 2); //8
-    console.log(result);
-    result = calculator.multiply(10, 2); //20
-    console.log(result);
-    result = calculator.divide(10, 2); //5
-    console.log(result);
-    result = calculator.square(10, 2); //100
-    console.log(result);
+//prompt();라는 함수는 사용자에게 창을 띄어 값을 받음.
+//prompt();를 사용하면 답을 할때까지 코드의 실행을 멈추고, 매우 오래된 방법임. 별로 안이쁨. css로 바꾸지도 못함.
+const age = prompt("how old are you?");
+console.log(typeof age);
+//typeof라는 키워드를 쓰면 type를 볼 수 있는데, prompt();에서 숫자를 입력해도 string이라고 뜬다. string이 디폴트이기 때문이다.
+
+//한 type로 받아서 다른 type로 바꾸는 작업을 해야한다. "15"-->15
+//string-->number로 변환해주는 함수 parseInt();
+//console.log(typeof "15", typeof parseInt("15")); --> string number
+//이렇게 숫자로 변환이 되야 비교를 할 수 있다.
+//참고로 "숫자"가 아닌게 입력되면 변환이 안됨. NaN(not a number)
+
+const age = parseInt(prompt("how old are you?"));
+console.log(age);
