8a3a3adb3519f68988373ff28f4772df21f6964f
민병준 <ehxhfl727@gmail.com>
2023-09-22 23:59:35 +0900

    CSS in Javascript_02

--- app.js
+++ app.js
@@ -2,14 +2,12 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const currentColor =h1.style.color;
-    let newColor;
-    if (h1.style.color === "blue"){
-        h1.style.color = "tomato";
-    }else{
-        h1.style.color ="blue";
-    }
-    h1.style.color =newColor;
+    const clickedClass = "clicked";
+    if(h1.className === "clickedClass") {
+        h1.className = "";
+        } else {
+        h1.className = "clickedClass";
+        }
 }
 
 
