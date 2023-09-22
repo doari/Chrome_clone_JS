4df1dca7a89defd2774a6b496e5152e8ddf8e8db
민병준 <ehxhfl727@gmail.com>
2023-09-23 00:39:10 +0900

    CSS in Javascript_03(toggle)

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
     const clickedClass = "clicked";
-    if(h1.className === "clickedClass") {
+    if(h1.className === clickedClass) {
         h1.className = "";
-        } else {
-        h1.className = "clickedClass";
-        }
+    }else {
+        h1.className = clickedClass;
+    }
 }
 
 
