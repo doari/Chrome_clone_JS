c310d478a153693a57e9473b5061239155fbc695
민병준 <ehxhfl727@gmail.com>
2023-09-27 01:17:21 +0900

    Events_02

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+
+const link = document.querySelector("a");
+
 function onLoginSubmit(event){
     event.preventDefault();
     console.log(event);
 }
 
+function handleLinkClick(event){
+    event.preventDefault();
+    console.log(loginInput.value);
+}
+
 loginForm.addEventListener("submit",onLoginSubmit);
+link.addEventListener("click",handleLinkClick);
+
+// addEventListener 안에 있는 함수는 직접 실행하지 않는다
+// 브라우저가 실행시켜주고
+// 브라우저에서 해당 이벤트에 대한 정보 즉, object를 가지게 된다.
+// addEventListener의 함수에서 object에 대한 자리만 할당해주면
+// 해당 이벤트가 발생시킨 정보들에 대한 object들을 볼 수 있다!
+// 이때 해당 이벤트가 가진 기본 Default값을 발생시키지 않기 하게 위해선 preventDefault를 이용하여 막을 수 있다!
