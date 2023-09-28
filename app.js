aa509343a5153acc8073c0ba2cb8cc394cee76b2
민병준 <ehxhfl727@gmail.com>
2023-09-29 01:01:49 +0900

    Getting Username(retouch)

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ function onLoginSubmit(event){
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     console.log(username);
-    greeting.innerText = "Helo " + username;
+//  greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -22,9 +23,6 @@ function handleLinkClick(event){
 loginForm.addEventListener("submit",onLoginSubmit);
 link.addEventListener("click",handleLinkClick);
 
-// addEventListener 안에 있는 함수는 직접 실행하지 않는다
-// 브라우저가 실행시켜주고
-// 브라우저에서 해당 이벤트에 대한 정보 즉, object를 가지게 된다.
-// addEventListener의 함수에서 object에 대한 자리만 할당해주면
-// 해당 이벤트가 발생시킨 정보들에 대한 object들을 볼 수 있다!
-// 이때 해당 이벤트가 가진 기본 Default값을 발생시키지 않기 하게 위해선 preventDefault를 이용하여 막을 수 있다!
+// "Hello" + username;
+// ===
+// `Hello ${username}`;
